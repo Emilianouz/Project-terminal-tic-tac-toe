a53313628c60effd14dcbe870f45ef1dc0773e59
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-09 14:44:23 +0100

    returns false if the array has more or less than 2 elements

--- __tests__/move-maker.test.js
+++ __tests__/move-maker.test.js
@@ -2,5 +2,13 @@ const validateMove=require("../move-maker.js")
 
 
 test("returns false if the aray doesn't contain just numbers",()=>{
-    expect(validateMove([1,"x"])).toEqual(false)
-})
+    expect(validateMove([1,"x"])).toEqual(false);
+    expect(validateMove(["x", 2])).toEqual(false);
+    expect(validateMove(["1", "2"])).toEqual(false);
+    expect(validateMove(["", 1])).toEqual(false);
+});
+test("returns false if the array has more or less than 2 elements",()=>{
+    expect(validateMove([1])).toEqual(false);
+    expect(validateMove([1, 2,3,8])).toEqual(false);
+    expect(validateMove([])).toEqual(false);
+});
