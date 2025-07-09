b0e5ea404b5ae5055ebda4e69c9d3071dfa2bbfc
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-09 15:21:40 +0100

    changed the way the files are connected

--- __tests__/move-maker.test.js
+++ __tests__/move-maker.test.js
@@ -1,23 +1,22 @@
-const validateMove=require("../move-maker.js")
+const validateMove = require("../move-maker.js");
 
-
-test("returns false if the aray doesn't contain just numbers",()=>{
-    expect(validateMove([1,"x"])).toEqual(false);
-    expect(validateMove(["x", 2])).toEqual(false);
-    expect(validateMove(["1", "2"])).toEqual(false);
-    expect(validateMove(["", 1])).toEqual(false);
+test("returns false if the aray doesn't contain just numbers", () => {
+  expect(validateMove([1, "x"])).toEqual(false);
+  expect(validateMove(["x", 2])).toEqual(false);
+  expect(validateMove(["1", "2"])).toEqual(false);
+  expect(validateMove(["", 1])).toEqual(false);
 });
-test("returns false if the array has more or less than 2 elements",()=>{
-    expect(validateMove([1])).toEqual(false);
-    expect(validateMove([1, 2,3,8])).toEqual(false);
-    expect(validateMove([])).toEqual(false);
+test("returns false if the array has more or less than 2 elements", () => {
+  expect(validateMove([1])).toEqual(false);
+  expect(validateMove([1, 2, 3, 8])).toEqual(false);
+  expect(validateMove([])).toEqual(false);
 });
-test("returns false if the numbers of the array are sequential ",()=>{
-    expect(validateMove([3,2,1])).toEqual(false);
-    expect(validateMove([1, 2,3])).toEqual(false);
-})
+test("returns false if the numbers of the array are sequential ", () => {
+  expect(validateMove([3, 2, 1])).toEqual(false);
+  expect(validateMove([1, 2, 3])).toEqual(false);
+});
 test("returns false if the numbers of the array are negative ", () => {
-  expect(validateMove([-1,3])).toEqual(false);
+  expect(validateMove([-1, 3])).toEqual(false);
   expect(validateMove([-1, -3])).toEqual(false);
   expect(validateMove([1, -3])).toEqual(false);
 });
