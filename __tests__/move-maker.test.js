8efb6ef0a48b2a8ed85c19a478e7fd4793ddd12c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-09 17:57:01 +0100

    returns false if any row inside the board hasn't got any free space

--- __tests__/move-maker.test.js
+++ __tests__/move-maker.test.js
@@ -1,24 +1,36 @@
 const validateMove = require("../move-maker.js");
+const board = [
+  ["X", "_", "_"],
+  ["_", "X", "_"],
+  ["O", "O", "X"],
+];
+validateMove([1, 2], board);
+  
 
 test("returns false if the aray doesn't contain just numbers", () => {
-  expect(validateMove([1, "x"])).toEqual(false);
-  expect(validateMove(["x", 2])).toEqual(false);
-  expect(validateMove(["1", "2"])).toEqual(false);
-  expect(validateMove(["", 1])).toEqual(false);
+  expect(validateMove([1, "x"],board)).toEqual(false);
+  expect(validateMove(["x", 2],board)).toEqual(false);
+  expect(validateMove(["1", "2"],board)).toEqual(false);
+  expect(validateMove(["", 1],board)).toEqual(false);
 });
 test("returns false if the array has more or less than 2 elements", () => {
-  expect(validateMove([1])).toEqual(false);
-  expect(validateMove([1, 2, 3, 8])).toEqual(false);
-  expect(validateMove([])).toEqual(false);
+  expect(validateMove([1],board)).toEqual(false);
+  expect(validateMove([1, 2, 3, 8],board)).toEqual(false);
+  expect(validateMove([],board)).toEqual(false);
 });
 test("returns false if the numbers of the array are sequential ", () => {
-  expect(validateMove([3, 2, 1])).toEqual(false);
-  expect(validateMove([1, 2, 3])).toEqual(false);
+  expect(validateMove([3, 2, 1],board)).toEqual(false);
+  expect(validateMove([1, 2, 3],board)).toEqual(false);
 });
 
 test("returns false if the numbers of the array are not in range 1-3 ", () => {
-  expect(validateMove([-1, 3])).toEqual(false);
-  expect(validateMove([-1, -3])).toEqual(false);
-  expect(validateMove([1, 4])).toEqual(false);
+  expect(validateMove([-1, 3],board)).toEqual(false);
+  expect(validateMove([-1, -3],board)).toEqual(false);
+  expect(validateMove([1, 4],board)).toEqual(false);
+});
+test("returns false if any row inside the board hasn't got any free space ", () => {
+    expect(validateMove([1, 3],[['X','X','X']])).toEqual(false);
+    expect(validateMove([1, 3], [["O", "O", "O"]])).toEqual(false);
+    expect(validateMove([1, 3], [["O", "X", "X"]])).toEqual(false);
 });
   
